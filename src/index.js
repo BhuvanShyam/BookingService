a7dca31e2ed1de1588f2b36bf68ee103521f0d4e
Bhuvan <bhuvanshyam2022@gmail.com>
2024-08-02 23:09:50 +0530

    added error files

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ const SetupAndStartServer = () => {
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+    
     if (process.env.DB_SYNC) {
       db.sequelize.sync({ alter: true });
     }
